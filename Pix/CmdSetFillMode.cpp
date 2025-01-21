75114e5f0067362b297a36951e486e48b8ad02b5
staap710 <146906243+staap710@users.noreply.github.com>
2025-01-21 15:15:27 -0800

    Update CmdSetFillMode.cpp

--- Pix/CmdSetFillMode.cpp
+++ Pix/CmdSetFillMode.cpp
@@ -9,15 +9,8 @@ bool CmdSetFillMode::Execute(const std::vector<std::string>& params)
 	}
 
 	FillMode fillMode;
-
-	if (params[0] == "wireframe")
-	{
-		fillMode = FillMode::Wireframe;
-	}
-	else if (params[0] == "solid")
-	{
-		fillMode = FillMode::Solid;
-	}
+	if (params[0] == "wireframe") { fillMode = FillMode::Wireframe; }
+	else if (params[0] == "solid") { fillMode = FillMode::Solid; }
 	else { return false; }
 
 	Rasterizer::Get()->SetFillMode(fillMode);
